0492d16bd55d4423f5eb9db681777a52b27dc7b4
Suo.JianFei <454667345@qq.com>
2018-05-22 11:23:26 +0800

    Update serverProxy_1.1.0.js

--- serverProxy_1.1.0.js
+++ serverProxy_1.1.0.js
@@ -4,33 +4,54 @@ const url = require('url');
 
 http.createServer(function (request, response) {
 
-	console.log("请求已接收。");
-	console.log("处理中...");
+    console.log("请求已接收。");
+    console.log("处理中...");
     var status = "Success";
-	var urlObj = url.parse(request.url);
-	try {
-		var param = urlObj.path.slice(1, urlObj.path.length);
-		var paramArr = param.split("=");
-		if (paramArr[0] === "address" && paramArr[1]) {
-			// open default browser
-			console.log("处理完成！");
-			console.log(paramArr[1]);
-			console.log("网页打开中...");
-			c.exec("start " + paramArr[1]);
-		} else {
-			status = "Failed";
-		}
-	
-	} catch (e) {
-		status = "Failed";
-	}
+    try {
+        var url_parts = url.parse(request.url,true);
+        var params = url_parts.query;
+        console.log("address: " + params.address);
+        console.log("browser: " + params.browser);
+        if (params.address) {
+            console.log("处理完成！");
+            console.log("网页打开中...");
+            try {
+                if (params.browser === "chrome") {
+                    // use chrome 
+                    console.log("open chrome browser");
+                    c.exec('start "" "chrome" "' + params.address + '"');
+                } else if (params.browser === "iexplore") {
+                    // use IE
+                    console.log("open iexplore browser");
+                    c.exec('start "" "iexplore" "' + params.address + '"');
+                } else {
+                    // use default browser
+                    console.log("open default browser");
+                    c.exec("start " + params.address);
+                }
+            } catch (e) {
+                try {
+                    // use default browser
+                    c.exec("start " + params.address);
+                } catch (e) {
+                    status = "Failed";
+                    console.log("open browser failed");
+                    console.log(e);
+                }
+            }
+        } else {
+            status = "Failed";
+        }
+    } catch (e) {
+        status = "Failed";
+    }
 
     // 发送 HTTP 头部 
     // HTTP 状态值: 200 : OK
     // 内容类型: text/plain
     response.writeHead(200, {'Content-Type': 'text/plain'});
     response.end(status);
-	console.log("全部处理结束！");
+    console.log("全部处理结束！");
 }).listen(9519);
 
 console.log("Server running at http://127.0.0.1:9519/");
